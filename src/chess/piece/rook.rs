d00f3b323a8b0ae55e036e59315d48d4bd290865
danilbushkov <danil1357997@gmail.com>
2022-07-25 20:38:15 +0300

    added get_moves for Rook

--- src/chess/piece/rook.rs
+++ src/chess/piece/rook.rs
@@ -1,4 +1,8 @@
 
+use crate::chess::crd::Crd;
+use crate::chess::board::Board;
+
+
 pub struct Rook {
     player: i8,
     first_move: bool,
@@ -17,7 +21,24 @@ impl Rook {
         self.player
     }
 
-    pub fn pub fn get_moves(&self, crd: &Crd, board: &Board) -> Vec<Crd> {
-        
+    pub fn get_moves(&self, crd: &Crd, board: &Board) -> Vec<Crd> {
+        let mut moves: Vec<Crd> = vec![];
+        let direction = [(1, 0),(0, 1),(-1, 0),(0, -1)];
+
+        for &(ref a, ref b) in &direction {
+            let mut search = true;
+            while search {
+                let crd = Crd::create(crd.x() + (*a as i8), crd.y() + (*b as i8));
+                
+                search = !board.is_piece(&crd);
+                if search || board.is_enemy_piece(&crd, self.player) {
+                    moves.push(crd.unwrap());
+                }
+            }
+
+        }
+
+
+        moves
     }
 }
