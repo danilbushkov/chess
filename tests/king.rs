4d4dcd47b0acbf23e71a9eb19b4cdae0b410815e
danilbushkov <danil1357997@gmail.com>
2022-08-02 18:04:19 +0300

    added new test for king

--- tests/king.rs
+++ tests/king.rs
@@ -1,6 +1,5 @@
 use chess::chess::board::Board;
 use std::collections::HashSet;
-use std::collections::HashMap;
 use chess::chess::crd::Crd;
     
 
@@ -31,6 +30,37 @@ fn king1() {
         Crd::create(3, 1).unwrap(),
         Crd::create(4, 2).unwrap(),
         Crd::create(5, 2).unwrap(),
+        ]);
+
+
+    assert_eq!(*moves, result);
+}
+#[test]
+fn king2() {
+    let board_usize: [[usize; 8]; 8] = 
+        [
+            [4, 6, 8, 0, 12,8, 6, 4],
+            [2, 2, 2, 2, 2, 2, 2, 2],
+            [0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 9, 0, 0, 0, 0],
+            [1, 0, 0, 0, 1, 1, 1, 1],
+            [3, 0, 0, 0, 11, 0, 0, 3],
+        ];
+
+    let mut board: Board = Board::create();
+    board.from(board_usize);
+
+    let moves = board.get_possible_moves(1);
+    let moves = moves.get(&Crd::create(7, 4).unwrap()).unwrap();
+
+    let result = HashSet::from([   
+        Crd::create(7, 6).unwrap(),
+        Crd::create(7, 5).unwrap(),
+        Crd::create(7, 3).unwrap(),
+        Crd::create(6, 3).unwrap(),
+        Crd::create(7, 1).unwrap(),
         ]);
 
 
